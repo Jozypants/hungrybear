7c1c5e5184415c9b3edd3d9a0a506874a2cc923e
Jozy Kinnaman <jozypants@gmail.com>
2020-04-21 11:34:20 -0700

    add a dynamic method for sleep and mood increase. Add tests for sleep decrementation and sleep method.

--- __tests__/hugrybear.test.js
+++ __tests__/hugrybear.test.js
@@ -7,6 +7,8 @@ describe("Fuzzy", () => {
   beforeEach(function () {
     fuzzy = new HungryBear("Fuzzy");
     fuzzy.setHunger();
+    fuzzy.gettingTired();
+    fuzzy.gettingAngry();
   });
 
   afterEach(function () {
@@ -17,24 +19,47 @@ describe("Fuzzy", () => {
     expect(fuzzy.name).toEqual("Fuzzy");
     expect(fuzzy.foodLevel).toEqual(10);
   });
-
+  
   test("should have a food level of 7 after 3001 milliseconds", () => {
     jest.advanceTimersByTime(3001);
     expect(fuzzy.foodLevel).toEqual(7);
   });
-    test("should get very hungry if the food level drops below zero", function () {
-      fuzzy.foodLevel = 0;
-      expect(fuzzy.didYouGetEaten()).toEqual(true);
-    });
+  
+  test("should get eaten if the food level is 0 or less or mood level rises aboce 20", function () {
+    fuzzy.moodLevel = 0;
+    fuzzy.foodLevel = 10;
+    expect(fuzzy.didYouGetEaten()).toEqual(false);
+  });
 
-    test("should get very hungry if 10 seconds pass without feeding", function () {
-      jest.advanceTimersByTime(10001);
-      expect(fuzzy.didYouGetEaten()).toEqual(true);
-    });
+  test("should get very hungry if 10 seconds pass without feeding", function () {
+    jest.advanceTimersByTime(10001);
+    expect(fuzzy.didYouGetEaten()).toEqual(true);
+  });
 
-    test("should have a food level of ten if it is fed", function () {
-      jest.advanceTimersByTime(9001);
-      fuzzy.feed();
-      expect(fuzzy.foodLevel).toEqual(10);
-    });
+  test ("should test moodlevel has caused us to be eaten by the beear.", () => {
+    jest.advanceTimersByTime(20001);
+    expect(fuzzy.didYouGetEaten()).toEqual(true);
+  });
+
+  test("should have a food level of ten if it is fed", function () {
+    jest.advanceTimersByTime(9001);
+    fuzzy.feed();
+    expect(fuzzy.foodLevel).toEqual(10);
+  });
+
+  test ("should test that the bear is getting tired over time", () => {
+    jest.advanceTimersByTime(10001);
+    expect(fuzzy.sleepLevel).toEqual(30);
+  });
+
+  test ("should test that the bear is getting angry over time", () => {
+    jest.advanceTimersByTime(20001);
+    expect(fuzzy.moodLevel).toEqual(20);
+  });
+
+  test("If bear doens't sleep increase mood", () => {
+    jest.advanceTimersByTime(20001);
+    fuzzy.sleepLevel = 0;
+    expect(fuzzy.isBearRested()).toEqual("you die");
+  });
 });
--- src/hungrybear.js
+++ src/hungrybear.js
@@ -2,22 +2,64 @@ export class HungryBear {
   constructor(name) {
     this.name = name;
     this.foodLevel = 10;
+    this.sleepLevel = 30;
+    this.moodLevel = 0;
   }
+
+// Decrementing Methods for Stats // 
 
   setHunger() {
     setInterval(() => {
       this.foodLevel--;
     }, 1000);
   }
-  didYouGetEaten() {
-    if (this.foodLevel > 0) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-
+  
   feed() {
     this.foodLevel = 10;
   }
+  
+  gettingTired() {
+    setInterval(() => {
+      this.sleepLevel--;
+    }, 30000);
+  }
+  
+  isBearRested() {
+    if (this.sleepLevel === 0) {
+      this.gettingAngry();
+      if (this.moodLevel >= 20) {
+        return "you die"
+      }
+    }
+    // if (this.sleepLevel === 0 ) {
+    //   this.moodLevel ++
+    // };
+  }
+  
+  gettingAngry() {
+    setInterval(() => {
+      this.moodLevel++;
+    }, 1000);
+  }
+  love() {
+    this.moodLevel = 0;
+  }
+  // Static Methods //
+  didYouGetEaten() {
+    if (this.foodLevel >0) {
+      return false;
+    } else  if (this.foodLevel <= 0){
+      return true;
+    }
+    if (this.moodLevel < 20) {
+      return false;
+    } else if (this.moodlevel >= 20){
+      return true;
+    }
+  }
 }
+
+// Sleep <= 0 = dead (20 secs)
+        // >= 20 = hibernating
+// Mood  >= 20 didYouGetEaten()= true 
+// Food <0 get eaten
