e97cfafc564fd192ab209200b2c08d10c4672281
Jozy Kinnaman <jozypants@gmail.com>
2020-04-21 11:58:56 -0700

    add css and html.

--- src/main.js
+++ src/main.js
@@ -2,3 +2,9 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 import $ from "jquery";
+import { Hungrybear } from "./hungrybear.js";
+
+
+$(document).ready(function() { 
+  $().click (function);
+}
